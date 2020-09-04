a567e1f28ead41b1ca929ba4d70e9d1e9aa5a080
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-03 23:36:51 -0400

    config sequelize

--- config/config.js
+++ config/config.js
@@ -1,3 +1,15 @@
+var config = require('./config');
+
+module.exports = {
+  database: config.database.name,
+  username: config.database.user,
+  password: config.database.pass,
+  dialect: 'postgres',
+  dialectModulePath: 'pg.js',
+  host: config.database.host,
+  port: config.database.port,
+  pool: config.database.pool
+};
 {
   "development": {
     "username": "root",
