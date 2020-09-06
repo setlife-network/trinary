ae9ea6f254e6d02435844035147c1d13753b47dc
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-06 13:09:10 -0400

    config

--- api/models/index.js
+++ api/models/index.js
@@ -1,37 +1,43 @@
-'use strict';
+const Sequelize = require('sequelize')
+const mysql2 = require('mysql2')
+const { MYSQL } = require('../../config/credentials');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const {
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME
+} = MYSQL
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+    DB_NAME,
+    DB_USERNAME,
+    DB_PASSWORD,
+    {
+        host: DB_HOST,
+        dialect: 'mysql',
+        port: 3306,
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    }
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+sequelize
+    .authenticate()
+    .then(function(err) {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(function (err) {
+      console.log('Unable to connect to the database:', err);
+    });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const db = {
+    sequelize,
+    models: {
+    }
+};
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+ module.exports = db;
