fe8dc94093a26df2f879f13f6aa13384e605a098
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-16 15:06:05 -0400

    Client model

---
+++ api/models/Client.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize) => {
+    
+    class Client extends Sequelize.Model {}
+
+    Client.init({
+        // Model attributes are defined here
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }, is_active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }, currency: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        // Model options go here
+        sequelize,
+        modelName: 'Client'
+    });
+
+    return Client
+
+}
