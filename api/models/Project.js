bb2fd18400f399340869d72945086503f8f9ea04
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-16 15:29:56 -0400

    Project model

---
+++ api/models/Project.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize) => {
+
+    class Project extends Sequelize.Model {}
+
+    Project.init({
+        // Model attributes are defined here
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        expected_budget: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        is_active: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        github_url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        client_id: { //FK
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Client,
+                key: 'id',
+            }
+        }
+    },
+    {
+        // Model options go here
+        sequelize,
+        modelName: 'Project'
+    });
+
+    return Project
+
+}
