722e504c536de8d0cb397952895a3c3340b242fe
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-16 15:27:43 -0400

    Time entry model

---
+++ api/models/TimeEntry.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize')
+
+module.exports = (sequelize) => {
+
+    class TimeEntry extends Sequelize.Model {}
+
+    Payment.init({
+        // Model attributes are defined here
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        seconds: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        start_time: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        toggl_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        contributor_id: { //FK
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Contributor,
+                key: 'id',
+            }
+        },
+        project_id: { //FK
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Project,
+                key: 'id',
+            }
+        }
+    },
+    {
+        // Model options go here
+        sequelize,
+        modelName: 'TimeEntry'
+    });
+
+    return TimeEntry
+
+}
