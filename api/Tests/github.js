8e3f23777b4ba13d17e6612dd3b1b2e8e995885e
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-21 16:41:58 -0400

    cleanup

--- api/Tests/github.js
+++ api/Tests/github.js
@@ -2,18 +2,6 @@ const github = require('../handlers/github')
 
 console.log('github');
 
-// github.fetchGithubTeamData({ auth_key: '1285b9d9dd6a5380008287218be71afa65cd0a98' })
-//     .then(response => {
-//         console.log('response');
-//         console.log(response);
-//     })
-
-// github.fetchGithubTeamRepos({ auth_key: '1285b9d9dd6a5380008287218be71afa65cd0a98' })
-//     .then(response => {
-//         console.log('user repos');
-//         console.log(response);
-//     })
-
 const userData = github.fetchUserData({ auth_key: '1285b9d9dd6a5380008287218be71afa65cd0a98' })
     .then(res => {
         console.log('res');
