e4a6d5a1739f373da917d750cab9b63abe32b4b3
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-22 12:49:14 -0400

    stripe test api call

--- api/handlers/stripe.js
+++ api/handlers/stripe.js
@@ -1,13 +1,24 @@
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripeAPI = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
-module.exports = {
+const stripe = module.exports = (() => {
 
-
-    const paymentIntent = (request) => {
-
+    const requestPaymentIntent = (params) => {
+        const paymentIntent = new Promise((resolve, reject) => {
+            stripeAPI.paymentIntents.create({
+                amount: 1000,
+                currency: 'usd',
+                payment_method_types: ['card'],
+                receipt_email: 'jenny.rosen@example.com',
+            })
+                .then(response => {
+                    resolve(response)
+                })
+                .catch(error => reject(new Error(error)))
+        })
+        return paymentIntent
     }
 
     return {
-        paymentIntent
+        requestPaymentIntent
     }
-}
+})();
