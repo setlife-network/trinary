1d7d764c1c4ddde7d9abd9cb7a6fd601fb3ce4e3
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-22 11:30:47 -0400

    cleanup

--- api/handlers/toggl.js
+++ api/handlers/toggl.js
@@ -3,20 +3,22 @@ const {
     TOGGL
 } = require('../../config/credentials')
 
-var togglClient = new TogglClient({apiToken: TOGGL.API_KEY});
-
+//var togglClient = new TogglClient({apiToken: TOGGL.API_KEY});
 
 const toggl = module.exports = (() => {
-    console.log('TOGGL.API_KEY');
-    console.log(TOGGL.API_KEY);
-    const fetchTimeEntries = new Promise((resolve, reject) => {
-        togglClient.getTimeEntries((err, timeEntries) => {
-            if (err){
-                reject(err)
-            }
-            resolve(timeEntries)
+
+    const fetchTimeEntries = (params) => {
+        const togglClient = new TogglClient({apiToken: params.accessToken});
+        return new Promise((resolve, reject) => {
+            togglClient.getTimeEntries((err, timeEntries) => {
+                if (err){
+                    reject(err)
+                }
+                resolve(timeEntries)
+            })
         })
-    })
+    }
+
 
     return {
         fetchTimeEntries
--- api/tests/connectToggl.js
+++ api/tests/connectToggl.js
@@ -1,9 +1,12 @@
 const toggl = require ('../handlers/toggl')
+const {
+    TOGGL
+} = require('../../config/credentials')
 
 console.log('toggl');
 console.log(toggl);
 console.log('toggl.createTimeEntry()');
-const timeEntries = toggl.fetchTimeEntries
+const timeEntries = toggl.fetchTimeEntries({accessToken: TOGGL.API_KEY})
     .then(res => {
         console.log(res);
     })
