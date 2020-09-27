54629214cbe0102762919585a595455dd49919b0
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-27 10:40:48 -0400

    Issue and Countributor schemas

---
+++ api/schema/types/ContributorType.js
@@ -0,0 +1,29 @@
+const gql = require('apollo-server')
+
+module.exports = gql`
+    scalar Date
+
+    type Contributor {
+        id: Int!
+        hourly_rate: Int
+        weekly_rate: Int
+        monthly_rate: Int
+        name: String!
+        date_created: Date!
+    }
+
+    type Query {
+        getContributors: [Contributor],
+        getContributor(id: Int!): Contributor
+    }
+
+    type Mutation {
+        createContributor(
+            hourly_rate: Int
+            weekly_rate: Int
+            monthly_rate: Int
+            name: String!
+            date_created: Date!
+        ): Contributor
+    }
+`
---
+++ api/schema/types/IssueType.js
@@ -0,0 +1,21 @@
+const gql = require('apollo-server')
+
+module.exports = gql`
+    type Issue {
+        id: Int!
+        github_url: String!
+        project: Project!
+    }
+
+    type Query {
+        getIssue(id: Int!): Issue,
+        getProjectIssues(projectId: Int!): [Issue]
+    }
+
+    type Mutation {
+        createIssue(
+            github_url: String!
+            project: Project!
+        ): Issue
+    }
+`
