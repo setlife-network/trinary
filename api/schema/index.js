baad0de415417face8608dea3c8646127d538bca
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-27 11:37:57 -0400

    types merged

--- api/schema/index.js
+++ api/schema/index.js
@@ -1,18 +1,32 @@
-const { makeExecutableSchema } = require('graphql-tools');
-const { mergeResolvers, mergeTypes } = require('merge-graphql-schemas');
+const { makeExecutableSchema, mergeResolvers, mergeTypes } = require('graphql-tools');
 //import resolvers
+
 //import types
+const AllocationType = require('./types/AllocationType')
+const ClientType = require('./types/ClientType')
+const ContributorType = require('./types/ContributorType')
+const IssueType = require('./types/IssueType')
+const PaymentType = require('./types/PaymentType')
+const ProjectType = require('./types/ProjectType')
+const TimeEntry = require('./types/TimeEntryType')
+
 //merge types
 const typeDefs = mergeTpes[(
-
+    AllocationType,
+    ClientType,
+    ContributorType,
+    IssueType,
+    PaymentType,
+    ProjectType,
+    TimeEntry
 )]
+
 //merge resolvers
 const resolvers = mergeResolvers([
 
 ])
 
-
 export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
