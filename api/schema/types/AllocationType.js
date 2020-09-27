77088b94c2d255f652d9531de52c5b3256e1abe6
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-09-27 10:22:01 -0400

    Allocation schema

---
+++ api/schema/types/AllocationType.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server')
+
+module.exports = gql`
+
+    scalar Date
+
+    "Define description"
+
+    type Allocation {
+        id: Int!
+        amount: Int!
+        rate_type: Int!
+        active: Boolean!
+        date_created: Date!
+        date_paid: Date!
+        payment: Payment
+        proyect: Proyect
+        contributor: Contributor
+    }
+
+    type Query {
+        getAllocations: [Allocation]
+        getAllocation(id: Int!): Allocation
+    }
+
+    type Mutation {
+        createAllocation(
+            amount: Int!,
+            rate_type: Int,
+            active: Boolean!,
+            date_created: Date!,
+            date_paid: Date!,
+            payment: Payment,
+            proyect: Proyect,
+            contributorId: Int!
+        ): Allocation
+    }
+
+`
