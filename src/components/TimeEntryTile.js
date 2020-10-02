d8facee984ce78269fa97912192b23f33dbf2191
Oscar Lafarga <otech47@gmail.com>
2020-10-02 08:52:59 -0400

    Add more placeholder components

---
+++ src/components/TimeEntryTile.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const TimeEntryTile = (props) => {
+
+    return (
+        <div className='TimeEntryTile'>
+            TimeEntryTile
+        </div>
+    );
+}
+
+TimeEntryTile.defaultProps = {
+    
+};
+
+export default TimeEntryTile;
