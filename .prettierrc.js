543ed1e0dba1b30d1c6729146700e395911a3c91
Oscar Lafarga <otech47@gmail.com>
2020-10-01 21:51:25 -0400

    Revert prettier trash

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'jsxSingleQuote': true,
-    'printWidth': 65,
-    'semi': false,
-    'singleQuote': true,
-    'quoteProps': 'preserve',
-    'trailingComma': 'es5',
-    'tabWidth': 4,
-}
