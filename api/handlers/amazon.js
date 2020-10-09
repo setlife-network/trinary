59d89943cb21f51f22247e0294d36b69497490ca
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-09 14:15:53 -0400

    cleanup

--- api/handlers/amazon.js
+++ api/handlers/amazon.js
@@ -3,11 +3,18 @@ const { AMAZON_AWS } = require('../config/credentials')
 
 const amazon = module.exports = (() => {
 
+    const bucket = 'project-trinary'
+
     const fetchFile = (params) => {
+        
+        const fileBucket = {
+            Bucket: bucket,
+            Key: params.file
+        }
 
         const s3 = new AWS.S3();
         return new Promise((resolve, reject) => {
-            s3.getObject(params, function(err, data) {
+            s3.getObject(fileBucket, function(err, data) {
                 if (err) reject(err, err.stack);
                 else {
                     data = data.Body.toString('utf8')
--- api/modules/paymentFiles.js
+++ api/modules/paymentFiles.js
@@ -3,17 +3,10 @@ const invoicelyCodebase = require('../scripts/invoicelyCodebase')
 
 const paymentFiles = module.exports = (() => {
 
-    //TODO: Move these constants somewhere else
-    const bucket = 'project-trinary'
     const invoiceFile = 'documents/payments/invoices-2019.csv'
 
-    const fileBucket = {
-        Bucket: bucket,
-        Key: invoiceFile
-    }
-
     const fetchCSV = () => {
-        amazon.fetchFile(fileBucket)
+        amazon.fetchFile({ file: invoiceFile })
             .then(res => {
                 invoicelyCodebase.modelCSV(res)
             })
