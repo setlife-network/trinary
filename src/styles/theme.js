12bcb785af169b197d0c448793265bfb409f6b69
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-21 12:37:02 -0400

    colors exported from colors.sccs closes #83

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,19 +1,18 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import colors from './colors.scss'
-import sizes from './sizes.scss'
 
-const theme_colors = {
-    setlife_blue: '#00C2D4',
-    light_grey: '#F1F1F1',
-}
+import {
+    setlifeBlue,
+    lightGrey
+} from './colors.scss'
+import sizes from './sizes.scss'
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: theme_colors.setlife_blue,
+            main: setlifeBlue,
         },
         secondary: {
-            main: theme_colors.light_grey,
+            main: lightGrey,
         },
     },
 })
