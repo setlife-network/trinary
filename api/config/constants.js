780d2e5f0a242d0b22cf191e95eda55a8b1abcf7
Oscar Lafarga <otech47@gmail.com>
2020-10-23 20:28:37 -0400

    Update constants.js

--- api/config/constants.js
+++ api/config/constants.js
@@ -4,6 +4,6 @@ module.exports = {
     GITHUB_OAUTH_URL: 'https://github.com/login/oauth/access_token',
     INVOICELY_CSV_PATH: `${process.env.INVOICELY_CSV_PATH}`,
     SITE_ROOT: process.env.NODE_ENV == 'production'
-        ? 'https://project-trinary.herokuapp.com/'
+        ? process.env.SITE_URL
         : 'http://localhost:6002'
 }
