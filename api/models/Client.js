05eda6415ba61d14d0ccbd322c8cccaa6ca9228e
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-27 14:46:56 -0400

    closes #88

--- api/models/Client.js
+++ api/models/Client.js
@@ -22,6 +22,10 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         currency: {
             type: DataTypes.STRING,
             allowNull: false
