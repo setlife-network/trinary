e84b51bb028042234f65fe56ce3c311cb5e68388
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-11-29 00:44:22 -0400

    UI polish and cleanup

--- src/pages/ClientsListPage.js
+++ src/pages/ClientsListPage.js
@@ -4,49 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import ClientsListManager from '../components/ClientsListManager'
 import ClientsList from '../components/ClientsList'
 
-const MOCKED_CLIENTS = [
-    {
-        name: 'Client A'
-    },
-    {
-        name: 'Client B'
-    },
-    {
-        name: 'Client C'
-    },
-]
-
-// Convert to imported component as ClientTile.js when ready to merge
-const ClientTile = (props) => {
-    const { client } = props
-
-    // Log the `client` object to confirm its data structure
-
-    return (
-        <div className='ClientTile'>
-            ClientTile
-        </div>
-    )
-
-}
-
 class ClientListPage extends React.Component {
-    componentDidMount() {}
-
-    renderClients = () => {
-        // TODO:
-        // fetch clients from API
-        // store them in state
-        // replace the mocked array
-
-        return MOCKED_CLIENTS.map(c => {
-            return (
-                <ClientTile
-                    client={c}
-                />
-            )
-        })
-    }
 
     render() {
         return (
