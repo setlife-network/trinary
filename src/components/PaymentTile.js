8b64c73c3672a1c30806d2ebce108d69bb8f78db
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-07 18:30:45 -0400

    PaymentTile as separated component

--- src/components/PaymentTile.js
+++ src/components/PaymentTile.js
@@ -1,16 +1,66 @@
 import React, { useState } from 'react';
+import moment from 'moment'
+import Box from '@material-ui/core/Box'
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
 
 const PaymentTile = (props) => {
+    const { payment } = props
+
+    const formattedDatePaid = moment.unix(payment.date_paid).format('MM/DD/YYYY')
+    const formattedDateIncurred = moment.unix(payment.date_incurred).format('MM/DD/YYYY')
+    const paymentHasBeenMade = payment.date_paid != null
 
     return (
-        <div className='PaymentTile'>
-            PaymentTile
-        </div>
-    );
+
+        <Box
+            className='PaymentTile'
+            flexDirection='row'
+            display='flex'
+        >
+            <Box flex={1}>
+                {`$ ${payment.amount}`}
+            </Box>
+
+            {paymentHasBeenMade
+                ? (
+                    <Box
+                        className='DatePaid'
+                        flex={2}
+                        flexDirection='row'
+                        display='flex'
+                    >
+                        <Box flex={1}>
+
+                            {`${formattedDatePaid}`}
+
+                        </Box>
+                        <MonetizationOnIcon className='MoneyIcon'/>
+                    </Box>
+                ) : (
+                    <Box
+                        className='DateIncurred'
+                        flex={2}
+                        flexDirection='row'
+                        display='flex'
+                    >
+                        <Box flex={1}>
+
+                            {`${formattedDateIncurred}`}
+
+                        </Box>
+                        <MonetizationOnIcon className='NoMoneyIcon'/>
+                    </Box>
+                )
+            }
+
+        </Box>
+
+    )
+
 }
 
 PaymentTile.defaultProps = {
-    
+
 };
 
 export default PaymentTile;
