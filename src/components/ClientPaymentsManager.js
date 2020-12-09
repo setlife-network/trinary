6365951c8edf308a97f9a5fc4daf8ce9c828669a
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-09 11:08:52 -0400

    not payments null UI bug fixed

--- src/components/ClientPaymentsManager.js
+++ src/components/ClientPaymentsManager.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import AddIcon from '@material-ui/icons/Add'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
+import {
+    AddIcon,
+    Box,
+    Button,
+    Grid,
+    Typography
+} from '@material-ui/core'
 import { gql, useQuery } from '@apollo/client'
 
 import { GET_CLIENT_TOTAL_PAID } from '../operations/queries/ClientQueries'
@@ -30,11 +32,7 @@ const ClientPaymentsManager = ({
     const { getClientById } = data
 
     return (
-        <Box
-            mt={3}
-            mx={1}
-            className='ClientPaymentsManager'
-        >
+        <Box mt={3} mx={1} className='ClientPaymentsManager'>
             <Grid
                 container
                 justify='space-between'
@@ -46,13 +44,19 @@ const ClientPaymentsManager = ({
                         </strong>
                     </Typography>
                 </Grid>
-                <Grid item>
-                    <Typography align='left' variant='h4'>
-                        <strong>
-                            {`${getClientById.totalPaid} ${getClientById.currency} Total`}
-                        </strong>
-                    </Typography>
-                </Grid>
+                {
+                    getClientById.totalPaid &&
+                    (
+                        <Grid item>
+                            <Typography align='left' variant='h4'>
+                                <strong>
+                                    {`${getClientById.totalPaid} ${getClientById.currency} Total`}
+                                </strong>
+                            </Typography>
+                        </Grid>
+                    )
+                }
+
             </Grid>
         </Box>
     )
