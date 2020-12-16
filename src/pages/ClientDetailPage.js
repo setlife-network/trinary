04d1d1e5dd3739367bd81bcdf2b0b9c97f8f7ef4
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-16 14:31:08 -0400

    ClientProjectsListManager replaced for ClientProjectsList

--- src/pages/ClientDetailPage.js
+++ src/pages/ClientDetailPage.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 
 import ClientInfo from '../components/ClientInfo'
-import ClientProjectsListManager from '../components/ClientProjectsListManager'
+import ClientProjectsList from '../components/ClientProjectsList'
 import ProjectsListManager from '../components/ProjectsListManager'
 import ClientPaymentsManager from '../components/ClientPaymentsManager'
 import ClientPaymentsListManager from '../components/ClientPaymentsListManager'
@@ -37,7 +37,7 @@ class ClientDetailPage extends React.Component {
                             clientId={clientId}
                             history={this.props.history}
                         />
-                        <ClientProjectsListManager
+                        <ClientProjectsList
                             clientId={clientId}
                             history={this.props.history}
                         />
