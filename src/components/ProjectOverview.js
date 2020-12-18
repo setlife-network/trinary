83b6a769a9238d659903bda1c36fdec37d6d3575
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-18 10:09:04 -0400

    toggle and github buttons

--- src/components/ProjectOverview.js
+++ src/components/ProjectOverview.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { Box, Grid, Typography } from '@material-ui/core'
+import {
+    Box,
+    Button,
+    Grid,
+    Typography
+} from '@material-ui/core'
 
 import { GET_PROJECT } from '../operations/queries/ProjectQueries'
 import ProjectSummary from './ProjectSummary'
@@ -31,9 +36,11 @@ const ProjectOverview = (props) => {
 
     const project = data?.getProjectById
 
+    console.log('project');
+    console.log(project);
+
     return (
         <Grid container className='ProjectOverview' justify='center'>
-
             <Grid item xs={10} lg={5}>
                 <Box p={3}>
                     <Typography variant='h4'>
@@ -43,7 +50,40 @@ const ProjectOverview = (props) => {
                     </Typography>
                 </Box>
                 <ProjectSummary project={project}/>
+                <Grid container>
+                    <Grid item xs={12} sm={6}>
+                        <Box mt={2} mr={{ xs: 0, sm: 1 }}>
+                            <Button
+                                fullWidth
+                                variant='outlined'
+                                color='primary'
+                                disabled={project.github_url ? 0 : 1}
+                                onClick={() => window.open(project.github_url, '_blank')}
+                            >
+                                <Typography variant='h6'>
+                                    {`Check Github profile`}
+                                </Typography>
+                            </Button>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Box mt={2} ml={{ xs: 0, sm: 1 }}>
+                            <Button
+                                fullWidth
+                                variant='outlined'
+                                color='primary'
+                                disabled={project.github_url ? 0 : 1}
+                                onClick={() => window.open(project.toggl_url, '_blank')}
+                            >
+                                <Typography variant='h6'>
+                                    {`Check Toggl profile`}
+                                </Typography>
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Grid>
+
             {
                 //<TimeTracking project={project}/>
             }
