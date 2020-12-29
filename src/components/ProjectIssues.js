ccfb86a024a78f548a701f2d5ca064b2b80aee91
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-29 16:22:20 -0400

    Issue query created

--- src/components/ProjectIssues.js
+++ src/components/ProjectIssues.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import {
+    Grid
+} from '@material-ui/core'
 
-const MOCKED_ISSUES = [
-    {
-        id: 1,
-        github_url: 'https://github.com/setlife-network/website/issues/1'
-    },
-    {
-        id: 2,
-        github_url: 'https://github.com/setlife-network/website/issues/2'
-    }
-]
-
+import { GET_PROJECT_ISSUES } from '../operations/queries/IssueQueries'
 // Convert to imported component as IssueTile.js when ready to merge
 const IssueTile = (props) => {
     const { issue } = props
-    
+
     // Log the `issue` object to confirm its data structure
 
     return (
@@ -27,31 +20,45 @@ const IssueTile = (props) => {
 
 const ProjectIssues = (props) => {
 
-    const renderIssues = () => {
-        // TODO:
-        // fetch issues from API
-        // store them in state
-        // replace the mocked array
+    // const renderIssues = () => {
+    //     // TODO:
+    //     // fetch issues from API
+    //     // store them in state
+    //     // replace the mocked array
+    //
+    //     return MOCKED_ISSUES.map(i => {
+    //         return (
+    //             <IssueTile
+    //                 issue={i}
+    //             />
+    //         )
+    //     })
+    // }
 
-        return MOCKED_ISSUES.map(i => {
-            return (
-                <IssueTile
-                    issue={i}
-                />
-            )
-        })
+    const { loading, error, data } = useQuery(GET_PROJECT_ISSUES, {
+        variables: { projectId: Number(clientId) }
+    })
+
+    if (loading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
     }
+    if (error) return `Error! ${error.message}`;
 
     return (
-        <div className='ProjectIssues'>
-            <h1>ProjectIssues</h1>
-            {renderIssues()}
-        </div>
+        <Grid container className='ProjectIssues'>
+            <h1>Issues</h1>
+            <Grid item>
+            </Grid>
+        </Grid>
     );
 }
 
 ProjectIssues.defaultProps = {
-    
+
 };
 
 export default ProjectIssues;
