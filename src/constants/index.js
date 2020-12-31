522f6d9b47711e6f7b05f308045fdfd2cc1b1bf5
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-12-31 15:49:59 -0400

    changes requested

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 export const LOGO_URL = 'https://project-trinary.s3.amazonaws.com/images/Logo.png'
 
-export const API_ROOT = process.env.NODE_ENV == 'production'
-    ? process.env.REACT_APP_API_URL
-    : 'http://localhost:6001/api'
+export const API_ROOT = process.env.REACT_APP_API_URL
 
 export const CURRENCIES = [
     {
