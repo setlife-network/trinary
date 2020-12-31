bf1f0413ef90ac8887ad8c092275f5657f24b157
Oscar Lafarga <otech47@gmail.com>
2020-12-31 03:41:44 -0500

    Fix typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const LOGO_URL = 'https://project-trinary.s3.amazonaws.com/images/Logo.png'
 
-export const API_URL = process.env.REACT_APP_API_URL
+export const API_ROOT = process.env.REACT_APP_API_URL
 
 export const CURRENCIES = [
     {
