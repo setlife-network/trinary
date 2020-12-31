06bbfc512191bd40d2843cc216d649e4b2921a1f
Oscar Lafarga <otech47@gmail.com>
2020-12-31 03:41:17 -0500

    Fix env vars and constants

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const SITE_ROOT = process.env.REACT_APP_SITE_URL
-
-export const API_ROOT = `${SITE_ROOT}/api`
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 export const LOGO_URL = 'https://project-trinary.s3.amazonaws.com/images/Logo.png'
-export const SITE_ROOT = process.env.NODE_ENV == 'production'
-    ? 'https://project-trinary.herokuapp.com/'
-    : 'http://localhost:6002'
-export const API_ROOT = `${SITE_ROOT}/api`
+
+export const API_URL = process.env.REACT_APP_API_URL
 
 export const CURRENCIES = [
     {
