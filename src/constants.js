97868b1c7c182eb24b7f984032a4220070da9619
Oscar Lafarga <otech47@gmail.com>
2020-12-31 03:32:13 -0500

    Fix env vars

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,3 @@
-export const SITE_ROOT = process.env.NODE_ENV == 'production'
-    ? 'https://project-trinary.herokuapp.com'
-    : 'http://localhost:6001'
+export const SITE_ROOT = process.env.REACT_APP_SITE_URL
 
 export const API_ROOT = `${SITE_ROOT}/api`
