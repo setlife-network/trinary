9d736c06f6130f3946835fabe812989d7a95e535
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-02-01 14:36:27 -0400

    formated all amount from add allocation

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -187,7 +187,7 @@ const AddProjectForm = ({
                         disabled={disableAdd}
                         onClick={createProject}
                     >
-                        Add Project
+                        {`Add Project`}
                     </Button>
                 </Box>
             </Grid>
