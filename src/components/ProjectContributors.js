ae4222a5b1165c8b672097555f191d4d4ba05d21
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-02-02 11:48:41 -0400

    closes #270

--- src/components/ProjectContributors.js
+++ src/components/ProjectContributors.js
@@ -4,7 +4,10 @@ import {
     Box,
     Grid
 } from '@material-ui/core/'
-import { differenceBy, filter } from 'lodash'
+import {
+    differenceBy,
+    filter
+} from 'lodash'
 import moment from 'moment'
 
 import { GET_PROJECT_CONTRIBUTORS } from '../operations/queries/ProjectQueries'
@@ -18,6 +21,7 @@ import GithubAccessBlocked from './GithubAccessBlocked'
 const ProjectContributors = (props) => {
 
     const { projectId } = props
+    const [contributors, setContributors] = useState([])
     const [openAddAllocationDialog, setOpenAddAllocationDialog] = useState(false)
     const [contributorClicked, setContributorClicked] = useState(null)
     const handleAddAllocationClose = (value) => {
@@ -51,10 +55,11 @@ const ProjectContributors = (props) => {
         errorPolicy: 'all'
     })
 
-    useEffect(() => {
-        var githubContributors = getGithubContributors({
+    useEffect(async () => {
+        const githubContributors = await getGithubContributors({
             variables: { project_id: Number(projectId) }
         })
+        setContributors(contributors.concat(...githubContributors.data.syncProjectGithubContributors))
     }, [])
 
     const addAllocation = (props) => {
@@ -88,11 +93,12 @@ const ProjectContributors = (props) => {
     const activeContributors = activeAllocations.map(a => {
         return a.contributor
     })
-    const { getContributors: contributors } = dataContributors
+    if (differenceBy(dataContributors.getContributors, contributors, 'id').length != 0) {
+        setContributors(contributors.concat(...dataContributors.getContributors))
+    }
     const contributorsToAdd = differenceBy(contributors, activeContributors, 'id')
 
     const renderContributors = (active, contributors) => {
-
         return contributors.map(c => {
             return (
                 <Grid item xs={12} md={6}>
@@ -139,7 +145,9 @@ const ProjectContributors = (props) => {
                 </Box>
                 <hr/>
             </Grid>
-            <h1>{`Add new contributors to the project`}</h1>
+            <h1>
+                {`Add new contributors to the project`}
+            </h1>
             <Grid item xs={12}>
                 <Box>
                     <Grid container>
