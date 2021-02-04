a800fde26965e8c44647c8892473f02d11e06535
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-02-04 14:29:29 -0400

    cleanup

--- src/scripts/selectors.js
+++ src/scripts/selectors.js
@@ -10,7 +10,7 @@ export const capitalizeWord = (props) => {
     return word.replace(
         /\w\S*/g,
         (txt) => {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
         }
     )
 }
