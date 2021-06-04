8257e05ba91f4a0dd00985a101112174f36c4a32
mecoccaro <miguele.coccaro@yahoo.com>
2021-06-04 13:08:22 -0400

    Close #395

--- api/handlers/stripe.js
+++ api/handlers/stripe.js
@@ -9,24 +9,25 @@ const stripe = module.exports = (() => {
     const stripeClient = stripeAPI(STRIPE.SECRET)
 
     const createClient = async (params) => {
-        const client = db.models.Client.findOne({
-            where: {
-                email: params.createFields.email
-            }
-        })
+        const client = await findClientWithEmail(params.createFields)
         if (!client.external_uuid) {
             return await stripeClient.customers.create({
                 email: params.createFields.email,
                 name: params.createFields.name,
             })
         }
+    }
 
-    const pushUpdatedClient = async (params) => {
-        const client = await db.models.Client.findOne({
+    const findClientWithEmail = async (params) => {
+        return await db.models.Client.findOne({
             where: {
-                email: params.updateFields.email
+                email: params.email
             }
         })
+    }
+
+    const pushUpdatedClient = async (params) => {
+        const client = await findClientWithEmail(params.updateFields)
         const stripe_uuid = client.external_uuid
         if (stripe_uuid) {
             return await stripeClient.customers.update(
