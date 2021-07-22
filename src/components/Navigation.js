bfbd96f3a1220610c699ed20bb89ca5bd3f5ad3e
Ovidio Rodriguez <ovifer02@gmail.com>
2021-07-22 16:14:27 -0400

    Title with fixed height

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,9 @@ import { useReactiveVar } from '@apollo/client'
 import {
     AppBar,
     Box,
-    Grid
+    Grid,
+    Typography,
+    makeStyles
 } from '@material-ui/core'
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 import { split } from 'lodash'
@@ -14,8 +16,20 @@ import { LOGO_URL, SMALL_LOGO_URL } from '../constants'
 import { capitalizeWord, matchTitlePage } from '../scripts/selectors'
 import { pageName } from '../reactivities/variables'
 
+const useStyles = makeStyles((theme) => ({
+    title: {
+        margin: '12px',
+        color: '#009BAD',
+        fontWeight: 'bold',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '1rem'
+        }
+    }
+}));
+
 const Navigation = (props) => {
 
+    const classes = useStyles()
     const history = useHistory()
     const redirectToHome = () => {
         history.push('/')
@@ -48,11 +62,15 @@ const Navigation = (props) => {
                         </Box>
                     </Grid>
                     <Grid item xs={8} sm={6}>
-                        <h2 className='navigation-title'>
+                        <Typography 
+                            variant='h5' 
+                            className={classes.title}
+                            noWrap
+                        >
                             {capitalizeWord({
                                 word: locationTitle.title || optionalLocationTitle
                             })}
-                        </h2>
+                        </Typography>
                     </Grid>
                 </Grid>
             </AppBar>
