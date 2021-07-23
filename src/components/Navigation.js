fada6ac0560ae1c186cf13ea8c3c42f7500c838d
Ovidio Rodriguez <ovifer02@gmail.com>
2021-07-23 17:45:38 -0400

    fix styles to scss file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,7 @@ import {
     AppBar,
     Box,
     Grid,
-    Typography,
-    makeStyles
+    Typography
 } from '@material-ui/core'
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 import { split } from 'lodash'
@@ -16,20 +15,8 @@ import { LOGO_URL, SMALL_LOGO_URL } from '../constants'
 import { capitalizeWord, matchTitlePage } from '../scripts/selectors'
 import { pageName } from '../reactivities/variables'
 
-const useStyles = makeStyles((theme) => ({
-    title: {
-        margin: '12px',
-        color: '#009BAD',
-        fontWeight: 'bold',
-        [theme.breakpoints.down('sm')]: {
-            fontSize: '1rem'
-        }
-    }
-}));
-
 const Navigation = (props) => {
 
-    const classes = useStyles()
     const history = useHistory()
     const redirectToHome = () => {
         history.push('/')
@@ -64,7 +51,7 @@ const Navigation = (props) => {
                     <Grid item xs={8} sm={6}>
                         <Typography 
                             variant='h5' 
-                            className={classes.title}
+                            className='navigation-title'
                             noWrap
                         >
                             {capitalizeWord({
