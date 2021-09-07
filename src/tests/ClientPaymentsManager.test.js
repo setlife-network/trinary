af31edcdc00b997b300ea242af3bb23cef7475fe
Ovidio Rodriguez <ovifer02@gmail.com>
2021-09-07 13:33:32 -0400

    improving render function

--- src/tests/ClientPaymentsManager.test.js
+++ src/tests/ClientPaymentsManager.test.js
@@ -5,24 +5,40 @@ import '@testing-library/jest-dom/extend-expect'
 import MockedProvider from '@apollo/client/testing'
 import { GET_CLIENT_TOTAL_PAID } from '../operations/queries/ClientQueries'
 
-const mockGetClientTotalPaid = [
-    {
-        request: {
-            query: GET_CLIENT_TOTAL_PAID,
-            variables: {
+const mockGetClientTotalPaid = {
+    request: {
+        query: GET_CLIENT_TOTAL_PAID,
+        variables: {
+            id: '40',
+            fromDate: null,
+            toDate: null
+        },
+    },
+    result: {
+        data: {
+            getClientById: {
                 id: '40',
-                fromDate: null,
-                toDate: null
+                currency: 'USD',
+                totalPaid(fromDate, toDate) 
             },
         },
-        result: {
-            data: {
-                getClientById: {
-                    id: '40',
-                    currency: 'USD',
-                    totalPaid(fromDate, toDate)
-                }
-            }
-        }
-    }
-]
+    },
+};
+
+// it('render clients by id', async () => {
+//     let wrapper;
+//     await act(async () => {
+//         wrapper = mount(
+//             <MockedProvider addTypename={false} mocks={mockGetClientTotalPaid}>
+//                 <ClientPaymentsManager />
+//             </MockedProvider>
+//         );
+//     });
+
+//     await act(() => wait(0));
+//     wrapper.update();
+//     expect(wrapper).toBeThruthy();
+//     expect(wrapper.find('.test')).toHaveText(
+
+//     );
+// })
