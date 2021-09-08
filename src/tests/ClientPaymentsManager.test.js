ee2571b5dbba0ae58305b076f203bf0565995087
Ovidio Rodriguez <ovifer02@gmail.com>
2021-09-08 15:03:42 -0400

    testing progress

--- src/tests/ClientPaymentsManager.test.js
+++ src/tests/ClientPaymentsManager.test.js
@@ -1,44 +1,54 @@
 import React from 'react'
-import ClientPaymentsManager from '../components/ClientPaymentsManager'
-import { act } from 'react-dom/test-utils'
+// import '@testing-library/jest-dom/extend-expect'
+// import MockedProvider from '@apollo/client/testing'
+// import { GET_ACTIVE_CLIENTS } from '../operations/queries/ClientQueries'
+import ClientTile from '../components/ClientTile'
+// import { act } from 'react-dom/test-utils'
+// import wait from 'waait'
+// import { configure, mount } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import MockedProvider from '@apollo/client/testing'
-import { GET_CLIENT_TOTAL_PAID } from '../operations/queries/ClientQueries'
 
-const mockGetClientTotalPaid = {
-    request: {
-        query: GET_CLIENT_TOTAL_PAID,
-        variables: {
-            id: '40',
-            fromDate: null,
-            toDate: null
-        },
-    },
-    result: {
-        data: {
-            getClientById: {
-                id: '40',
-                currency: 'USD',
-                totalPaid: '485000'
-            },
-        },
-    },
-};
-
-it('render clients by id', async () => {
-    let wrapper;
-    await act(async () => {
-        wrapper = mount(
-            <MockedProvider addTypename={false} mocks={mockGetClientTotalPaid}>
-                <ClientPaymentsManager />
-            </MockedProvider>
-        );
-    });
-
-    await act(() => wait(0));
-    wrapper.update();
-    expect(wrapper).toBeThruthy();
-    expect(wrapper.find('.test')).toHaveText(
-
-    );
+test('Client name', () => {
+    const component = render(<ClientTile/>);
+    const clientName = component.getByTestId('client-name')
+    
+    expect(clientName).toBeTruthy()
 })
+
+// configure({ adapter: new Adapter() });
+
+// const mockGetClients = {
+//     request: {
+//         query: GET_ACTIVE_CLIENTS,
+//     },
+//     result: {
+//         data: {
+//             getClients: {   
+//                 id: 35,
+//                 name: 'Created From Trinary',
+//                 currency: 'USD',
+//                 is_active: true
+//             },
+//         },
+//     },
+// };
+
+// it('render clients by id', async () => {
+//     let wrapper;
+//     await act(async () => {
+//         wrapper = mount(
+//             <MockedProvider addTypename={false} mocks={mockGetClients}>
+//                 <ClientTile />
+//             </MockedProvider>
+//         );
+//     });
+
+//     await act(() => wait(0));
+//     wrapper.update();
+//     expect(wrapper).toBeTruthy();
+//     expect(wrapper.find('.client-name')).toHaveText(
+//         'Create From Trinary'
+//     );
+// })
