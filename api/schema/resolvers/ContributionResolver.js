b17f933256ed4181da4febf4d593c93520eba52d
Ovidio Rodriguez <ovifer02@gmail.com>
2021-10-19 12:38:18 -0400

    Contribution resolver

---
+++ api/schema/resolvers/ContributionResolver.js
@@ -0,0 +1,44 @@
+module.exports = {
+    Contribution: {
+        contributor: (permission, args, { models }) => {
+            return models.Contributor.findByPk(permission.contributor_id)
+        },
+        issue: (permission, args, { models }) => {
+            return models.Issue.findByPk(permission.issue_id)
+        }
+    },
+    Query: {
+        getContributions: (root, args, { models }) => {
+            return models.Contribution.findAll()
+        },
+        getContributionsByContributorId: (root, { id }, { models }) => {
+            return models.Contribution.findAll({
+                where: {
+                    contributor_id: id
+                }
+            })
+        },
+        getContributionsByIssueId: (root, { id }, { models }) => {
+            return models.Contribution.findAll({
+                where: {
+                    issue_id: id
+                }
+            })
+        },
+        getContributionByContributorIdAndIssueId: (root, { contributor_id, issue_id }, { models }) => {
+            return models.Contribution.findOne({
+                where: {
+                    contributor_id,
+                    issue_id
+                }
+            })
+        },
+    },
+    Mutation: {
+        createContribution: (root, { createFields }, { models }) => {
+            return models.Contribution.create({
+                ...createFields
+            })
+        },
+    }
+}
