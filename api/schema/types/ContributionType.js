2732211c48f5facf51e475d8548ae81c07457719
Ovidio Rodriguez <ovifer02@gmail.com>
2021-10-19 12:56:06 -0400

    Contribution type creation

---
+++ api/schema/types/ContributionType.js
@@ -0,0 +1,32 @@
+const { gql } = require('apollo-server')
+
+module.exports = gql`
+
+    type Contribution {
+        id: Int!
+        contributor_id: Int!
+        issue_id: Int!
+        is_author: Int!
+        is_assigned: Int!
+        contributor: Contributor!
+        issue: Issue!
+    }
+    type Query {
+        getContributions: [Contribution]
+        getContributionsByContributorId(id: Int!): [Contribution]
+        getContributionsByIssuId(id: Int!): [Contribution]
+        getContributionByContributorIdAndIssueId(
+            contributor_id: Int!
+            issue_id: Int!
+        ): Contribution
+    }
+    input CreateContributionInput {
+        contributor_id: Int!
+        issue_id: Int!
+        is_author: Int!,
+        is_assigned: Int!
+    }
+    type Mutation {
+        createContribution(createFields: CreateContributionInput): Contribution
+    }
+    `
