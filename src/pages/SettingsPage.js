4bcd30e07309b2e28181fc2ea3120a2ce9cffb80
Ovidio Rodriguez <ovifer02@gmail.com>
2021-11-16 12:12:41 -0400

    Creation of SettingsPage

---
+++ src/pages/SettingsPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+class SettingsPage extends React.Component {
+
+    render() {
+        return (
+            <Grid container>
+                <Grid auto>
+                    {'Settings page'}
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default SettingsPage
