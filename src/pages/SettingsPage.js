6d6e01b245317c9f180370544c147cb99ca2e98d
Ovidio Rodriguez <ovifer02@gmail.com>
2021-11-16 14:17:43 -0400

    Add Logout component to SettingsPage

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import {
+    Grid
+} from '@material-ui/core'
+
+import Logout from '../components/Logout'
+import { pageName } from '../reactivities/variables'
 
 class SettingsPage extends React.Component {
 
     render() {
+        
+        pageName('Settings')
+
         return (
-            <Grid container>
-                <Grid auto>
-                    {'Settings page'}
+            <Grid container className='Settings'>
+                <Grid xs={12}>
+                    <Logout/>
                 </Grid>
             </Grid>
         )
