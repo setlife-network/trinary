2f6f98a7ba42aa3f2fc8bf2b8092c29d3f7b06f6
Ovidio Rodriguez <ovifer02@gmail.com>
2021-12-16 14:27:15 -0400

    Add useQuery for active projects

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import Grid from '@material-ui/core/Grid'
 import { orderBy } from 'lodash'
 
 import ProjectTile from './ProjectTile'
+import LoadingProgress from './LoadingProgress'
+import { GET_ACTIVE_PROJECTS } from '../operations/queries/ProjectQueries';
 
 const ProjectsList = ({
-    history,
-    projects
+    history
 }) => {
+
+    const { 
+        loading: loadingActiveProject,
+        error: errorActiveProject, 
+        data: dataActiveProject 
+    } = useQuery(GET_ACTIVE_PROJECTS, {
+        fetchPolicy: 'cache-and-network'
+    });
+
+    if (loadingActiveProject) return <LoadingProgress />
+    if (errorActiveProject) return `Error! ${errorActiveProject.message}`;
+    const projects = orderBy(dataActiveProject.getActiveProjects, 'is_active', 'desc')
 
     return (
         projects.map(p => {
