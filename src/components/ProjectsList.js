c804f76133e08892048d2f8711a44a9a95c21eb1
Ovidio Rodriguez <ovifer02@gmail.com>
2021-12-21 13:01:41 -0400

    revert ProjectList to have projects prop

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import { useQuery } from '@apollo/client';
 import Grid from '@material-ui/core/Grid'
-import { orderBy } from 'lodash'
 
 import ProjectTile from './ProjectTile'
-import LoadingProgress from './LoadingProgress'
-import { GET_ACTIVE_PROJECTS } from '../operations/queries/ProjectQueries';
 
 const ProjectsList = ({
-    history
+    history,
+    projects
 }) => {
-
-    const { 
-        loading: loadingActiveProject,
-        error: errorActiveProject, 
-        data: dataActiveProject 
-    } = useQuery(GET_ACTIVE_PROJECTS, {
-        fetchPolicy: 'cache-and-network'
-    });
-
-    if (loadingActiveProject) return <LoadingProgress />
-    if (errorActiveProject) return `Error! ${errorActiveProject.message}`;
-    const projects = orderBy(dataActiveProject.getActiveProjects, 'is_active', 'desc')
 
     return (
         projects.map(p => {
