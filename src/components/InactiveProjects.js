4b324120aea8220037567746db59a23ac1a9f49b
Ovidio Rodriguez <ovifer02@gmail.com>
2021-12-21 12:51:23 -0400

    InactiveProjects component

---
+++ src/components/InactiveProjects.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+
+import InactiveProjectListManager from './InactiveProjectListManager';
+import HomeInactiveProjects from './HomeInactiveProjects';
+import ClientInactiveProjectLists from './ClientInactiveProjectsList';
+
+const InactiveProjects = ({
+    history,
+    home,
+    clientId
+}) => {
+
+    const [showInactiveProjects, setShowInactiveProjects] = useState(false)
+
+    return (
+        <>
+            <InactiveProjectListManager
+                clientId={clientId}
+                showInactiveProjects={showInactiveProjects}
+                setShowInactiveProjects={setShowInactiveProjects}
+            />
+            {showInactiveProjects
+                ? (home
+                    ? (
+                        <Grid container>
+                            <HomeInactiveProjects
+                                history={history}
+                            />
+                        </Grid>
+                    ) : (
+                        <Grid container>
+                            <ClientInactiveProjectLists
+                                clientId={clientId}
+                                history={history}
+                            />
+                        </Grid>
+                    )
+                ) : null
+            }
+        </>
+    )
+}
+
+export default InactiveProjects
