5fcb2a6822ef949f636f81dd3e484eaa962dcfed
Oscar Lafarga <otech47@gmail.com>
2022-05-23 15:18:03 +0200

    Fix github auth conditional and add test command for args

--- src/tests/e2e/roles.js
+++ src/tests/e2e/roles.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
-import { Selector, Role } from 'testcafe';
+import { ClientFunction, Selector, Role } from 'testcafe';
+
+const getWindowLocation = ClientFunction(() => window.location);
 
 export const testUser = Role(`${process.env.SITE_URL}/login`, async t => {
     const trinaryLoginButton = Selector('a').withAttribute('data-testid', 'login-button')
@@ -14,10 +16,16 @@ export const testUser = Role(`${process.env.SITE_URL}/login`, async t => {
         .typeText('#password', process.env.TESTCAFE_ROLE_GITHUB_PASSWORD)
         .click(githubSignInButton)
 
-    // Uncomment the code below if Github token expires and requires
+    // Need the code below if Github token expires and requires
     // the authorization step to proceed with authentication
+    const location = await getWindowLocation()
+    
+    if (location.host.includes('github.com')) {
+        const githubAuthorizeButton = Selector('#js-oauth-authorize-btn')
 
-    // const githubAuthorizeButton = Selector('#js-oauth-authorize-btn')
-    // await t.wait(2000).click(githubAuthorizeButton);
+        if (githubAuthorizeButton?.exists) {
+            await t.click(githubAuthorizeButton);
+        }
+    }
 
 });
