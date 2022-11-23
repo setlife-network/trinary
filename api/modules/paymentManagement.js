5f3336fa9bb8a675453d4ad2902df2b605f26d2d
Rishabh <rishkwal@gmail.com>
2022-11-23 15:00:06 +0530

    Allow BTC invoice generation without Client

--- api/modules/paymentManagement.js
+++ api/modules/paymentManagement.js
@@ -22,10 +22,14 @@ const paymentManagement = module.exports = (() => {
     }
 
     const processBitcoinInvoiceCreation = async (paymentId) => {
+        let paymentCurrency
+        let isClientCurrencyBtc = false
+        
         const payment = await db.models.Payment.findByPk(paymentId)
         const { 
             amount, 
             client_id, 
+            currency,
             date_paid, 
             external_uuid, 
             external_uuid_type 
@@ -33,18 +37,26 @@ const paymentManagement = module.exports = (() => {
 
         if (date_paid) throw new Error(`Payment has already been made`)
 
-        const client = await db.models.Client.findByPk(client_id)
-        const isClientCurrencyBtc = 
-            client.dataValues.currency === `BTC` ||
-            client.dataValues.currency === `SATS`
-        
+        if (client_id) {
+            const client = await db.models.Client.findByPk(client_id)
+            paymentCurrency = client.dataValues.currency
+            isClientCurrencyBtc = 
+            paymentCurrency === `BTC` ||
+            paymentCurrency === `SATS`
+        }
+
+        if (currency) {
+            isClientCurrencyBtc = currency === `BTC` || currency === `SATS`
+            paymentCurrency = currency
+        }
+
         if (!isClientCurrencyBtc) {
             throw new Error(`Client's currency is not Bitcoin`)
-        } 
-
+        }
+        
         const convertedAmount = Number(amount / 100)
 
-        const amountInSats = client.dataValues.currency === `BTC` 
+        const amountInSats = paymentCurrency === `BTC` 
             ? Number((convertedAmount * 100000000).toFixed(0)) // 100M sats = 1 BTC
             : convertedAmount
 
