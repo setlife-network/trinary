2eea8f18ab5f5b2918652242fd6cf43b96497bd0
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-11-23 14:41:32 -0400

    add handle in dashboard

--- srcv2/pages/DashboardPage.js
+++ srcv2/pages/DashboardPage.js
@@ -19,7 +19,7 @@ const DashboardPage = () => {
             <Section backgroundColor={'bg-white'} className={'rounded-b-[70px]'}>
                 <div className='grid grid-flow-row auto-rows-max gap-8'>
                     <p className='text-3xl text-center font-bold'>
-                        {`Welcome, ${sessionUser().name}`}
+                        {`Welcome, @${sessionUser().github_handle.split('/').pop()}`}
                     </p>
                     <div className='rounded bg-setlife grid grid-flow-row auto-rows-max gap-2 px-8 py-4'>
                         {!!sessionUser().totalPaid &&
