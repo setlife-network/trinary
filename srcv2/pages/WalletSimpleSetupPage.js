47dc73d17424d898d63a7044ba29ca123c14911b
Sofia Rodriguez <sofiaromorales@gmail.com>
2023-05-09 16:48:06 -0400

    simple wallet setup UI

--- srcv2/pages/WalletSimpleSetupPage.js
+++ srcv2/pages/WalletSimpleSetupPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Icon
 } from '@material-ui/core'
@@ -6,6 +6,9 @@ import {
 import Section from '../components/Section'
 
 const WalletSimpleSetupPage = () => {
+
+    const [btcAddress, setBtcAddress] = useState('')
+
     return (
         <div className='WalletSimpleSetupPage h-full min-h-screen'>
             <Section backgroundColor={'bg-white-light'} className={'h-full min-h-screen'}>
@@ -17,8 +20,43 @@ const WalletSimpleSetupPage = () => {
                         Enter your info below to set up your wallet
                     </p>
                     <div className='info'>
-                        <Icon className='icon fas fa-info-circle text-black my-auto' fontSize='medium'/>
+                        <Icon
+                            className='icon fas fa-info-circle text-black my-auto'
+                            fontSize='medium'
+                            onClick={() => console.log('open modal (TODO)')}
+                        />
                     </div>
+                </div>
+                <div className='mt-12'>
+                    <input 
+                        type='text'
+                        placeholder='Paste BTC wallet address'
+                        onChange={(e) => setBtcAddress(e.target.value)}
+                        className='
+                            form-control
+                            block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-black
+                            font-normal
+                            bg-white bg-clip-padding
+                            border border-solid border-light
+                            rounded-lg
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                        '
+                    />
+                </div> 
+                <div className='grid absolute bottom-10 left-16 right-16 gap-4'>
+                    <button type='button'>
+                        Cancel
+                    </button>
+                    <button type='button' className='bg-setlife rounded-full py-2 w-full text-white font-bold'>
+                        Set Up Wallet
+                    </button>
                 </div>
             </Section>
         </div>
