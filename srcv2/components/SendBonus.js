45f32c7385062292aae90a73dd2b893410af4ac9
Sofia Rodriguez <sofiaromorales@gmail.com>
2023-05-31 10:06:20 -0400

    bonus type switch UI

--- srcv2/components/SendBonus.js
+++ srcv2/components/SendBonus.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const BONUS_SPLIT_TYPES = [
+    {
+        type: 'equaly',
+        name: 'Split Equaly'
+    },
+    {
+        type: 'customize',
+        name: 'Customize'
+    }
+]
 
 const SendBonus = () => {
+
+    const [selectedBonusSplitType, setSelectedBonusSplitType] = useState(0)
+
     return (
         <div className='SendBonus'>
             <p className='font-bold text-xl mb-4'>
                 Enter info below to send a bonus
             </p>
             <div className='rounded-full bg-white-light grid grid-cols-2'>
-                <button type='button' className='bg-med-gray font-bold rounded-full mr-1 ml-2 my-2 py-1 px-2'>
-                    Split equaly
-                </button>
-                <button type='button' className='bg-med-gray font-bold rounded-full mr-2 ml-1 my-2 py-1 px-2'>
+                <button
+                    type='button'
+                    className={`font-bold rounded-full mr-1 ml-2 my-2 py-1 px-2 ${selectedBonusSplitType == 0 ? 'bg-setlife text-white' : 'bg-med-gray'}`}
+                    onClick={() => setSelectedBonusSplitType(0)}
+                >
                     <p>
-                        Customize
+                        {BONUS_SPLIT_TYPES[0].name}
+                    </p>
+                </button>
+                <button
+                    type='button'
+                    className={`bg-med-gray font-bold rounded-full mr-2 ml-1 my-2 py-1 px-2 ${selectedBonusSplitType == 1 ? 'bg-setlife text-white' : 'bg-med-gray'}`}
+                    onClick={() => setSelectedBonusSplitType(1)}
+                >
+                    <p>
+                        {BONUS_SPLIT_TYPES[1].name}
                     </p>
                 </button>
             </div>
